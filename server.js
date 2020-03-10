4552a5de333e933ecc411fe7802b423f1c1acebd
shadab <sadabmajeed@gmail.com>
2020-03-10 16:56:05 +0530

    first comit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+const port=process.env.PORT||3000
+app.listen(port,()=>{
+   console.log("server started") 
+})
+const path=require('path')
+app.use('/public',express.static(path.join(__dirname,'public')))
+app.get('/',(req,res)=>{res.send("hello from backed")})
+
